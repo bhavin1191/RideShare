1f31e5d6f6308da6395765e5d083e57e3a9c6eec
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-16 14:59:39 -0400

    Added spring boot configuration

--- RideShare/src/main/java/edu/nyu/cloud/bootstrap/Application.java
+++ RideShare/src/main/java/edu/nyu/cloud/bootstrap/Application.java
@@ -1,33 +1,23 @@
 package edu.nyu.cloud.bootstrap;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * This class is used to bootstrap the application.
  * 
  * @author rahulkhanna Date:05-Apr-2016
  */
-// @EnableAutoConfiguration
-// @ComponentScan(basePackages = "edu.nyu.cloud")
+@SpringBootApplication
+@ComponentScan(basePackages = "edu.nyu.cloud")
+@ImportResource({"classpath:*/hibernate-config.xml","classpath:*/hibernate-beans.xml","classpath:*/application-beans.xml"})
 public class Application {
 
-	private static Application app = new Application();
-
-	private static Application getInstance() {
-		return app;
-	}
-
 	public static void main(String[] args) {
-		// SpringApplication.run(Application.class, args);
-		getInstance().propel();
+		SpringApplication.run(Application.class, args);
 
 	}
 
-	private void propel() {
-		while (!Thread.currentThread().isInterrupted()) {
-			ApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/application-context.xml");
-		}
-
-	}
 }
