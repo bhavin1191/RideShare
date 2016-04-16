74c43b622a964d678efe9c75f45c2062a6a7e330
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-16 15:04:20 -0400

    Added NewRideDaoImpl class

---
+++ RideShare/src/main/java/edu/nyu/cloud/newride/dao/db/hibernate/NewRideDaoImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.nyu.cloud.newride.dao.db.hibernate;
+
+import edu.nyu.cloud.newride.dao.db.NewRideDao;
+
+/**
+ * @author rahulkhanna
+ * Date:06-Apr-2016
+ */
+public class NewRideDaoImpl implements NewRideDao {
+
+}
