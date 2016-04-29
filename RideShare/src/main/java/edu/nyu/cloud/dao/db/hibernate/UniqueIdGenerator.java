88097d5a1e8ce1a02c0d6a14d8254cf5b08a2f07
Rahul khanna <rahul.khanna@nyu.edu>
2016-04-29 10:59:19 -0400

    Inclusion of ID generator and transaction management.

--- RideShare/src/main/java/edu/nyu/cloud/dao/db/hibernate/UniqueIdGenerator.java
+++ RideShare/src/main/java/edu/nyu/cloud/dao/db/hibernate/UniqueIdGenerator.java
@@ -6,6 +6,10 @@ package edu.nyu.cloud.dao.db.hibernate;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Projections;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.nyu.cloud.dao.db.IDGenerator;
 
@@ -14,12 +18,13 @@ import edu.nyu.cloud.dao.db.IDGenerator;
  * 
  * @author rahulkhanna Date:03-Apr-2016
  */
+@Transactional
 public class UniqueIdGenerator implements IDGenerator {
 
 	private static final Logger LOG = Logger.getLogger(UniqueIdGenerator.class.getName());
 	private AtomicLong currentId;
 	private final String entityName;
-
+	
 	/**
 	 * @param entityName
 	 *            for which ID will be generated.
@@ -33,6 +38,8 @@ public class UniqueIdGenerator implements IDGenerator {
 	public void initialize(long value) {
 		LOG.info("Initializing ID generator for entity "+getEntityName());
 		currentId = new AtomicLong();
+		//Criteria criteria = session.getCurrentSession().createCriteria(getEntityName()).setProjection(Projections.max("id"));
+//		long value = (Long)criteria.uniqueResult();
 		currentId.set(value);
 		LOG.info("Initialized "+getEntityName()+" with ID "+currentId.get());
 	}
