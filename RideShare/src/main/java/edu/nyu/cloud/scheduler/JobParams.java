bc5eb8106e0b7f8f7a8bf01b1d615d1d7c90c580
Rahul khanna <rahul.khanna@nyu.edu>
2016-05-05 17:37:00 -0400

    Various Jobs Params

---
+++ RideShare/src/main/java/edu/nyu/cloud/scheduler/JobParams.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.nyu.cloud.scheduler;
+
+/**
+ * @author rahulkhanna
+ * Date:02-May-2016
+ */
+public enum JobParams {
+
+	SOURCE, DESTINATION, USER_ID, USER_PROFILE, INCOMING_POOL_REQUEST
+	
+}
