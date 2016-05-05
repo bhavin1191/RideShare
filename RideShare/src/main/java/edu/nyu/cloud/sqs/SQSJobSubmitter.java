d2e65244d72b972f4e2c9f6fb625af30dfd411df
Rahul khanna <rahul.khanna@nyu.edu>
2016-05-05 17:37:23 -0400

    SQS Module Impl

---
+++ RideShare/src/main/java/edu/nyu/cloud/sqs/SQSJobSubmitter.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package edu.nyu.cloud.sqs;
+
+import com.google.gson.Gson;
+
+import edu.nyu.cloud.service.beans.IncomingPoolRequest;
+
+/**
+ * @author rahulkhanna Date:03-May-2016
+ */
+public class SQSJobSubmitter {
+
+	private final String accessToken;
+	private final String accessSecret;
+	private final String consumerKey;
+	private final String consumerSecret;
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public String getAccessSecret() {
+		return accessSecret;
+	}
+
+	public String getConsumerKey() {
+		return consumerKey;
+	}
+
+	public String getConsumerSecret() {
+		return consumerSecret;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param accessToken
+	 * @param accessSecret
+	 * @param consumerKey
+	 * @param consumerSecret
+	 */
+	public SQSJobSubmitter(String accessToken, String accessSecret, String consumerKey, String consumerSecret) {
+		this.accessToken = accessToken;
+		this.accessSecret = accessSecret;
+		this.consumerKey = consumerKey;
+		this.consumerSecret = consumerSecret;
+	}
+
+	public void sendJobRequest(IncomingPoolRequest request) {
+		Gson gson = new Gson();
+		String poolRequest = gson.toJson(request);
+		
+	}
+
+}
