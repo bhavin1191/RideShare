b380155529cc2db23a4c1c1d0ea4917cc40555b6
Rahul khanna <rahul.khanna@nyu.edu>
2016-05-12 06:49:42 -0400

    Added a variable.

--- RideShare/src/main/java/edu/nyu/cloud/service/beans/NewRideSharingRequest.java
+++ RideShare/src/main/java/edu/nyu/cloud/service/beans/NewRideSharingRequest.java
@@ -4,51 +4,63 @@
 package edu.nyu.cloud.service.beans;
 
 /**
- * This class represents a new request in search for ride. 
+ * This class represents a new request in search for ride.
  * 
- * @author rahulkhanna
- * Date:05-Apr-2016
+ * @author rahulkhanna Date:05-Apr-2016
  */
 public class NewRideSharingRequest {
-	
+
 	private final String userId;
 	private final String source;
 	private final String destination;
 	private final String date;
-	
-	public NewRideSharingRequest(){
-		this(null,null,null,null);
+	private int numberOfPassengers;
+
+	public NewRideSharingRequest() {
+		this(null, null, null, null, 0);
 	}
-	
+
 	/**
-	 * Constructor 
+	 * Constructor
 	 * 
 	 * @param userId
 	 * @param source
 	 * @param destination
 	 * @param date
 	 */
-	public NewRideSharingRequest(String userId, String source, String destination, String date) {
+	public NewRideSharingRequest(String userId, String source, String destination, String date,
+			int numberOfSeatsRequired) {
 		super();
 		this.userId = userId;
 		this.source = source;
 		this.destination = destination;
 		this.date = date;
+		this.numberOfPassengers = numberOfSeatsRequired;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public String getDestination() {
 		return destination;
 	}
+
 	public String getDate() {
 		return date;
 	}
-	
-	
-	
-	
+
+	public int getNumberOfPassengers() {
+		return numberOfPassengers;
+	}
+
+	public void setNumberOfPassengers(int numberOfPassengers) {
+		this.numberOfPassengers = numberOfPassengers;
+	}
+
+		
 }
