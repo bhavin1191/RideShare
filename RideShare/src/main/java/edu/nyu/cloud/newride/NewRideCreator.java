b01a79d31ecd9fc4937d3879e20b67a70773c0de
Rahul khanna <rahul.khanna@nyu.edu>
2016-05-12 06:06:27 -0400

    Integrated SQS.

--- RideShare/src/main/java/edu/nyu/cloud/newride/NewRideCreator.java
+++ RideShare/src/main/java/edu/nyu/cloud/newride/NewRideCreator.java
@@ -3,25 +3,19 @@
  */
 package edu.nyu.cloud.newride;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-
-import com.uber.sdk.rides.client.error.ApiException;
 
 import edu.nyu.cloud.beans.NewRide;
 import edu.nyu.cloud.beans.SerializableLatLng;
 import edu.nyu.cloud.beans.UberRide;
-import edu.nyu.cloud.beans.UserProfile;
 import edu.nyu.cloud.dao.db.IDGenerator;
 import edu.nyu.cloud.newride.dao.db.NewRideDao;
 import edu.nyu.cloud.newride.dao.db.UberRideDao;
-import edu.nyu.cloud.service.beans.IncomingPoolRequest;
 import edu.nyu.cloud.service.beans.EmailNotification;
-import edu.nyu.cloud.service.beans.EmailService;
-import edu.nyu.cloud.user.dao.db.hibernate.*;
+import edu.nyu.cloud.service.beans.IncomingPoolRequest;
+import edu.nyu.cloud.sqs.SQSJobSubmitter;
 
 /**
  * This class is used to create a new ride which will be shared by other users.
@@ -30,99 +24,69 @@ import edu.nyu.cloud.user.dao.db.hibernate.*;
  */
 public class NewRideCreator {
 
-	private final NewRideDao dao;	
+	private final NewRideDao dao;
 	private final IDGenerator routeIDGenerator;
-	private final UberRideDao uberdao;
 	private final EmailNotification emailNotification;
+	private final SQSJobSubmitter uberRideRequestGenerator;
 
 	/**
 	 * Constructor
 	 * 
 	 * @param dao
 	 */
-	public NewRideCreator(NewRideDao dao, IDGenerator routeIDGenerator,
-			UberRideDao uberdao, EmailNotification emailNotification) {
+	public NewRideCreator(NewRideDao dao, IDGenerator routeIDGenerator,EmailNotification emailNotification, SQSJobSubmitter uberRideRequestGenerator ) {
 		super();
-		this.dao = dao;		
-		this.routeIDGenerator = routeIDGenerator;		
-		this.uberdao = uberdao;
+		this.dao = dao;
+		this.routeIDGenerator = routeIDGenerator;
 		this.emailNotification = emailNotification;
+		this.uberRideRequestGenerator = uberRideRequestGenerator;
 	}
 
 	public void createNewRideForPool(IncomingPoolRequest newPoolRequest) {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		Date date;
-		String emailBody = null;		
+		String emailBody = null;
 		try {
 			date = format.parse(newPoolRequest.getDate());
 		} catch (ParseException e) {
 			throw new RuntimeException(e);
 		}
-		
-		if(newPoolRequest.getCarType().toLowerCase().equals("mycar"))
-		{
+
+		if (newPoolRequest.getCarType().toLowerCase().equals("mycar")) {
 			NewRide ride = new NewRide(newPoolRequest.getUserId(), newPoolRequest.getSource(),
-					newPoolRequest.getDestination(), date, newPoolRequest.getSelectRoute());
+					newPoolRequest.getDestination(), date, newPoolRequest.getSelectRoute(),
+					newPoolRequest.getNumberOfPassengers());
 			long newRouteId = routeIDGenerator.getNewId();
 			for (SerializableLatLng serializableLatLng : ride.getSelectedRoute().getLatlng()) {
 				serializableLatLng.setRouteId(newRouteId);
-				
-				}
-		
-			System.out.println("Route id for new ride :"+newRouteId);
+
+			}
+
+			System.out.println("Route id for new ride :" + newRouteId);
 			ride.getSelectedRoute().setId(newRouteId);
 			dao.saveNewRide(ride);
-			emailBody = constructEmailBody(ride,null);
-			emailNotification.sendConfirmationEmail(newPoolRequest.getUserId(), emailBody); // Send confirmation email for non uber ride.
-			
-		}
-		else if(newPoolRequest.getCarType().toLowerCase().equals("uber"))
-		{
-			
-			NewUberRide newUber = null;
-			try {
-				newUber = new NewUberRide();
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			List<UberRide> uberRide = null;
-			try {
-				uberRide = newUber.confirmRide(newPoolRequest.getSource(),newPoolRequest.getDestination(),newPoolRequest.getNumberOfPassengers());
-			} catch (ApiException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}			
-			uberdao.saveUberRide(uberRide.get(0));
-			emailBody = constructEmailBody(null,uberRide.get(0));
+			emailBody = constructEmailBody(ride, null);
 			emailNotification.sendConfirmationEmail(newPoolRequest.getUserId(), emailBody);
+		} else {
+			uberRideRequestGenerator.sendJobRequest(newPoolRequest);
+			emailNotification.sendConfirmationEmail(newPoolRequest.getUserId(), "Your ride has been scheduled");
 		}
-		
-		
+
 	}
-	
-	public String constructEmailBody( NewRide ride, UberRide uberRide)
-	{
+
+	public String constructEmailBody(NewRide ride, UberRide uberRide) {
 		StringBuilder emailBody = new StringBuilder();
-		if(ride != null)
-		{
-			emailBody.append("Destination : "+ ride.getDestination() + "\n");
+		if (ride != null) {
+			emailBody.append("Destination : " + ride.getDestination() + "\n");
 			emailBody.append("Ride ID :" + ride.getId() + "\n");
-			emailBody.append("Time of Trip :"+ ride.getTimeOfTrip() +"\n");
+			emailBody.append("Time of Trip :" + ride.getTimeOfTrip() + "\n");
 			emailBody.append("Requested by :" + ride.getRequester() + "\n");
-		}
-		else if(uberRide != null)
-		{
+		} else if (uberRide != null) {
 			emailBody.append("Passengers :" + uberRide.getCapacity() + "\n");
-			emailBody.append("Uber Ride ID :" +uberRide.getId() +"\n");
+			emailBody.append("Uber Ride ID :" + uberRide.getId() + "\n");
 			emailBody.append("Uber Product :" + uberRide.getProductdisplayname() + "\n");
-			emailBody.append("Ride Id :" +uberRide.getRideid()+ "\n");
-		}
-		else
-		{
+			emailBody.append("Ride Id :" + uberRide.getRideid() + "\n");
+		} else {
 			emailBody.append("Ride created successfully we will provide details shortly");
 		}
 		return emailBody.toString();
